d1c5b5cf2f38c2e5c3d61b5e232453bd6f651484
shukobayashidmm <shu.kobayashi-0627@yahoo.ne.jp>
2023-03-13 07:58:39 +0000

    firstcommit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'users/top'
+  get 'books/top'
+  root to: "homes#top"
+  get 'homes/about' => 'homes#about'
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
