42add1e6353e3ac392aa5fc01396dc61612960f2
shukobayashidmm <shu.kobayashi-0627@yahoo.ne.jp>
2023-03-16 01:32:38 +0000

    部分テンプレート

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,10 +5,16 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+       @book = Book.new(book_params)
+       @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have creatad book successfully."
+      redirect_to book_path(@book)
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
@@ -28,15 +34,22 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to books_path(book.id)
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      flash[:notice]="Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
-     book = Book.find(params[:id])
-     book.destroy
-     redirect_to books_path
+       @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice]="Book was successfully destroyed."
+      redirect_to books_path
+    end
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+
+
+
   def show
     @user = User.find(params[:id])
     @book = Book.new
